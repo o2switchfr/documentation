3b379359bcc5f9c7c1a63b9f1ee4afd9f1808837
Alexis Madrzejewski <alexis@madrzejewski.com>
2024-09-30 20:26:25 +0200

    web redirects

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -45,7 +45,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/o2switchfr/documentation/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/o2switchfr/documentation/edit/main/',
           routeBasePath: '/',
         },
         blog: false,
